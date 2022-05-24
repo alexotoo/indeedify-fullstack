5d25303ae4bd74802e42664c7958a829994a7b8a
Alex Otoo <alexotoous@outlook.com>
2022-05-24 18:51:51 -0400

    initial commit

--- server.js
+++ server.js
@@ -4,4 +4,6 @@ import { createServer } from "http";
 const PORT = process.env.PORT || 8000;
 const server = createServer(app);
 
-server.listen(PORT, () => console.log(`server listening on port: ${PORT}...`));
+server.listen(PORT, () =>
+  console.log(`server listening on port: ${PORT}...!!`)
+);
