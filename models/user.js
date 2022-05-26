68c713b543362c9aea0d59a9b345e527524456ec
Alex Otoo <alexotoous@outlook.com>
2022-05-26 12:32:09 -0400

    jwt created

--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import validator from "validator";
 const { Schema } = mongoose;
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const UserSchema = new Schema({
   name: {
@@ -42,15 +44,15 @@ const UserSchema = new Schema({
 UserSchema.pre("save", async function () {
   // console.log(this.modifiedPaths())
   // if (!this.isModified("password")) return;
-  // const salt = await bcrypt.genSalt(10);
-  // this.password = await bcrypt.hash(this.password, salt);
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
-// UserSchema.methods.createJWT = function () {
-//   return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
-//     expiresIn: process.env.JWT_LIFETIME,
-//   });
-// };
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
+};
 
 // UserSchema.methods.comparePassword = async function (candidatePassword) {
 //   const isMatch = await bcrypt.compare(candidatePassword, this.password);
